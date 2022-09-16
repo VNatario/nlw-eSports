e3803d00657cc21d7086e3eb3cb3fbc8ece348ef
Vitor Natario <natariovitor@gmail.com>
2022-09-16 11:29:17 -0300

    feat: adding text with gradient

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -3,8 +3,16 @@ import logo from "./assets/logo-nlw.svg";
 
 function App() {
   return (
-    <div className="w-8 h-8 bg-black dark:bg-slate-500">
+    <div className="max-w-[1344px] mx-auto flex flex-col items-center my-20">
       <img src={logo} alt="logo nlw" />
+
+      <h1 className="text-6xl text-white font-black mt-20">
+        Seu{" "}
+        <span className="text-transparent bg-nlw-gradient bg-clip-text">
+          duo
+        </span>{" "}
+        está aqui.
+      </h1>
     </div>
   );
 }
