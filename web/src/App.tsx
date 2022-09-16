4559572fb6d115f7c7ad9e2c88bcd559e4da66ee
Vitor Natario <natariovitor@gmail.com>
2022-09-16 11:11:08 -0300

    feat: adding backgroundImg and logo

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,7 +1,12 @@
 import "./styles/main.css";
+import logo from "./assets/logo-nlw.svg";
 
 function App() {
-  return <div className="w-8 h-8 bg-violet-500"></div>;
+  return (
+    <div className="w-8 h-8 bg-black dark:bg-slate-500">
+      <img src={logo} alt="logo nlw" />
+    </div>
+  );
 }
 
 export default App;
