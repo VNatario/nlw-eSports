893b9e57ca1158d96deacb9a0c8aeb7f4ad303f6
Vitor Natario <natariovitor@gmail.com>
2022-09-16 18:03:35 -0300

    feat: create static carossel

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -13,6 +13,28 @@ function App() {
         </span>{" "}
         está aqui.
       </h1>
+
+      <div className="grid grid-cols-6 gap-6 mt-16">
+        <a href="">
+          <img src="/image-1.png" alt="" />
+        </a>
+
+        <a href="">
+          <img src="/image-2.png" alt="" />
+        </a>
+        <a href="">
+          <img src="/image-3.png" alt="" />
+        </a>
+        <a href="">
+          <img src="/image-4.png" alt="" />
+        </a>
+        <a href="">
+          <img src="/image-5.png" alt="" />
+        </a>
+        <a href="">
+          <img src="/image-6.png" alt="" />
+        </a>
+      </div>
     </div>
   );
 }
