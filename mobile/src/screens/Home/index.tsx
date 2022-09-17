25333bed7aa01dc3c26b5113a573e2ad333f027b
Vitor Natario <natariovitor@gmail.com>
2022-09-16 22:42:17 -0300

    feat: create Heading

--- mobile/src/screens/Home/index.tsx
+++ mobile/src/screens/Home/index.tsx
@@ -1,7 +1,19 @@
-import { View } from "react-native";
+import { Image, View, Text } from "react-native";
+
+import logoImg from "../../assets/logo-nlw-esports.png";
+import { Heading } from "../../components/Heading";
 
 import { styles } from "./styles";
 
 export function Home() {
-  return <View style={styles.container}></View>;
+  return (
+    <View style={styles.container}>
+      <Image source={logoImg} style={styles.logo} />
+
+      <Heading
+        title="Encontre seu duo!"
+        subtitle="Selecione o game que deseja jogar..."
+      />
+    </View>
+  );
 }
