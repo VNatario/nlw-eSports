a7b16425c8ab5c9812725f3e53ee150456ad198b
Vitor Natario <natariovitor@gmail.com>
2022-09-16 23:25:27 -0300

    feat: create GameCard

--- mobile/src/screens/Home/index.tsx
+++ mobile/src/screens/Home/index.tsx
@@ -1,7 +1,11 @@
-import { Image, View, Text } from "react-native";
+import { Inter_500Medium } from "@expo-google-fonts/inter";
+import { Image, View, Text, FlatList } from "react-native";
 
 import logoImg from "../../assets/logo-nlw-esports.png";
+import { GameCard } from "../../components/GameCard";
 import { Heading } from "../../components/Heading";
+
+import { GAMES } from "../../utils/games";
 
 import { styles } from "./styles";
 
@@ -13,6 +17,15 @@ export function Home() {
       <Heading
         title="Encontre seu duo!"
         subtitle="Selecione o game que deseja jogar..."
+      />
+
+      <FlatList
+        data={GAMES}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => <GameCard data={item} />}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={styles.contentList}
       />
     </View>
   );
