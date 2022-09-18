de9034dc2267a64f01b1f141135855f4ab01a756
Vitor Natario <natariovitor@gmail.com>
2022-09-18 20:48:58 -0300

    feat: intal prisma/client

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,8 @@
 import express from "express";
+import { PrismaClient } from "@prisma/client";
 
 const app = express();
+const prisma = new PrismaClient();
 app.listen(3333, () => console.log("Rodando na porta 3333..."));
 
 app.get("/games", (request, response) => {
