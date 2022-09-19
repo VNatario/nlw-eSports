6d12b597cdfcf78788a914de5d81bab471cec8d6
Vitor Natario <natariovitor@gmail.com>
2022-09-18 21:02:49 -0300

    feat: return list games

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,11 +2,22 @@ import express from "express";
 import { PrismaClient } from "@prisma/client";
 
 const app = express();
-const prisma = new PrismaClient();
+const prisma = new PrismaClient({
+  log: ["query"],
+});
 app.listen(3333, () => console.log("Rodando na porta 3333..."));
 
-app.get("/games", (request, response) => {
-  return response.json([]);
+app.get("/games", async (request, response) => {
+  const games = await prisma.game.findMany({
+    include: {
+      _count: {
+        select: {
+          ads: true,
+        },
+      },
+    },
+  });
+  return response.json(games);
 });
 
 app.post("/ads", (request, response) => {
